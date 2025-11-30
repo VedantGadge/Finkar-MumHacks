250d4aa7d63a0f9c59a9bdb5cdbd6b7b386dc073
Vedant Gadge <vedant.gadgegsis@gmail.com>
2025-11-30 22:26:18 +0530

    chnages for app

--- frontend/finkar-web/capacitor.config.ts
+++ frontend/finkar-web/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'finkar-web',
+  appName: 'FinKar',
   webDir: 'build'
 };
 
